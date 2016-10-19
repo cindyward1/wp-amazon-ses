1ea4af6e13952560823ec3bb0e5678e052ff0ab3
Michael Lippold <michael@trialguides.com>
2016-10-19 12:03:35 -0700

    Send attachments

--- classes/amazon-ses.php
+++ classes/amazon-ses.php
@@ -56,6 +56,57 @@ class Amazon_SES extends AWS_Plugin_Base {
 	}
 
 	/**
+	 * Header value filter
+	 *
+	 * @param array() $headers
+	 * @param string  $filter_string
+	 *
+	 * @return array  matching strings
+	 */
+	private function filter_headers( $headers, $filter_string ) {
+		return array_filter( $headers, function( $value ) use ( $filter_string ) {
+			return substr( $value, 0, strlen( $filter_string ) ) === $filter_string;
+		} );
+	}
+
+	/**
+	 * Get From address from header or return WordPress default
+	 *
+	 * @param array() $headers
+	 *
+	 * @return string from
+	 */
+	private function from( $headers ) {
+		$addresses = $this->filter_headers( $headers, 'From: ' );
+		return empty( $addresses ) ? get_option( 'admin_email' ) : substr( array_shift( $addresses ), 5 );
+	}
+
+	/**
+	 *
+	 * Get content type for message body
+	 *
+	 * @param array() $headers
+	 *
+	 * @return string Content-Type
+	 */
+	private function content_type( $headers ) {
+		$content_types = $this->filter_headers( $headers, 'Content-Type: ' );
+		return empty( $content_types ) ? 'Content-Type: text/plain' : array_shift( $content_types );
+	}
+
+	/**
+	 * Get charset for message body
+	 *
+	 * @param array() $headers
+	 *
+	 * @return string charset
+	 */
+	private function charset( $headers ) {
+		$charsets = $this->filter_headers( $headers, 'charset' );
+		return empty( $charsets ) ? 'charset=UTF-8' : array_shift( $charsets );
+	}
+
+	/**
 	 * Send an email using Amazon SES
 	 *
 	 * @param string $to       the recipient for email message
@@ -65,30 +116,68 @@ class Amazon_SES extends AWS_Plugin_Base {
 	 *
 	 * @return bool  success
 	 */
-	public function mail( $to, $subject, $message, $headers = '' ) {
-		try {
-			if ( is_string( $to ) ) :
-				$to = array( $to );
-			endif;
+	public function mail( $to, $subject, $message, $headers = '', $attachments ) {
+		if ( is_string( $to ) ) :
+			$to = array( $to );
+		endif;
 
-			$this->get_sesclient( self::DEFAULT_REGION )->sendEmail( array(
-				'Source' => get_option( 'admin_email' ),
-				'Destination' => array(
-					'ToAddresses' => $to
-				),
-				'Message' => array(
-					'Subject' => array(
-						'Data' => $subject,
-						'Charset' => 'UTF-8'
+		try {
+			if ( empty( $attachments ) ) :
+				$this->get_sesclient( self::DEFAULT_REGION )->sendEmail( array(
+					'Source' => from( $headers ),
+					'Destination' => array(
+						'ToAddresses' => $to
 					),
-					'Body' => array(
-						'Text' => array(
-							'Data' => $message,
+					'Message' => array(
+						'Subject' => array(
+							'Data' => $subject,
 							'Charset' => 'UTF-8'
+						),
+						'Body' => array(
+						       'Text' => array(
+								'Data' => $message,
+								'Charset' => $this->charset( $headers )
+							)
 						)
 					)
-				)
-			) );
+				) );
+			else :
+				$boundary = uniqid( rand(), true );
+
+				$raw_message = 'To: ' . join( ', ', $to ) . "\n";
+				$raw_message .= 'From: ' . $this->from( $headers ) . "\n";
+				$raw_message .= 'Subject: ' . $subject . "\n";
+				$raw_message .= 'MIME-Version: 1.0' . "\n";
+				$raw_message .= 'Content-type: Multipart/Mixed; boundary="' . $boundary . '"' . "\n";
+				$raw_message .= "\n--{$boundary}\n";
+				$raw_message .= 'Content-type: Multipart/Alternative; boundary="alt-' . $boundary . '"' . "\n";
+
+				if ( $message != null && strlen( $message ) > 0) :
+					$raw_message .= "\n--alt-{$boundary}\n";
+					$raw_message .= $this->content_type( $headers ) . '; ' . $this->charset( $headers ) . "\n\n";
+					$raw_message .= $message . "\n";
+				endif;
+
+				$raw_message .= "\n--{$boundary}\n";
+
+				foreach( $attachments as $attachment ) :
+					 $raw_message .= "\n--{$boundary}\n";
+					 $raw_message .= 'Content-Type: application/octet-stream; name="' . basename( $attachment ) . '"' . "\n";
+					 $raw_message .= 'Content-Disposition: attachment' . "\n";
+					 $raw_message .= 'Content-Transfer-Encoding: base64' . "\n";
+					 $raw_message .= "\n" . chunk_split( base64_encode( file_get_contents( $attachment ) ), 76, "\n" ) . "\n";
+				endforeach;
+
+				$raw_message .= "\n--{$boundary}--\n";
+
+				$this->get_sesclient( self::DEFAULT_REGION )->sendRawEmail( array(
+					'Source' => get_option( 'admin_email' ),
+					'Destinations' => $to,
+					'RawMessage' => array(
+						'Data' => base64_encode( $raw_message )
+					)
+				) );
+			endif;
 
 			return true;
 		} catch( Exception $e ) {
--- include/functions.php
+++ include/functions.php
@@ -5,11 +5,11 @@
 
 if ( ! function_exists( 'wp_mail' ) ) :
 
-	function wp_mail( $to, $subject, $message, $headers = '' ) {
+	function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
 		global $amazon_ses;
 
 		if ( isset( $amazon_ses ) ) {
-			return $amazon_ses->mail( $to, $subject, $message, $headers );
+			return $amazon_ses->mail( $to, $subject, $message, $headers, $attachments );
 		} else {
 			return false;
 		}
